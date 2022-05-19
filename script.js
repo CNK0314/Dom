4fdcd6d28db4a634b9cf7ed516d03ff5c1d117fa
Chanakarn <chanakarn_h@cmu.ac.th>
2022-05-19 09:43:22 +0700

    element

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-var newText = 'new Text'
-document.getElementById('ok').innerHTML = newText
+// var newText = 'new Text'
+// document.getElementById('ok').innerHTML = newText
 
-var newName = 'Chanakarn'
-document.getElementById('no').innerHTML = '<bi><i>' + newName + '</i></bi>';
+// var newName = 'Chanakarn'
+// document.getElementById('no').innerHTML = '<bi><i>' + newName + '</i></bi>';
+
+document.getElementById('no').addEventListener('mousemove', function(e) {
+    console.log('x = ', e.screenX, " y = ",  e.screenY)
+})
+let eleS = document.getElementsByClassName('flex-item')
+for (let ele of eleS) {
+    ele.addEventListener('mousemove', function(e) {
+        console.log('x = ', e.screenX, " y = ",  e.screenY)
+    })
+    ele.addEventListener('click', function(e) {
+        alert(ele.innerText)
+    })
+}
