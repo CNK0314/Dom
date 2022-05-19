b3e45489b1f21a712312b6a627ac3fd7db40de7e
Chanakarn <chanakarn_h@cmu.ac.th>
2022-05-19 11:42:52 +0700

    ex 1 margin

--- script.js
+++ script.js
@@ -4,15 +4,43 @@
 // var newName = 'Chanakarn'
 // document.getElementById('no').innerHTML = '<bi><i>' + newName + '</i></bi>';
 
-document.getElementById('no').addEventListener('mousemove', function(e) {
-    console.log('x = ', e.screenX, " y = ",  e.screenY)
+// document.getElementById('no').addEventListener('mousemove', function(e) {
+//     console.log('x = ', e.screenX, " y = ",  e.screenY)
+// })
+// let eleS = document.getElementsByClassName('flex-item')
+// console.log(eleS)
+// for (let ele of eleS) {
+//     ele.addEventListener('mousemove', function(e) {
+//         console.log('x = ', e.screenX, " y = ",  e.screenY)
+//     })
+//     ele.addEventListener('click', function(e) {
+//         alert(ele.innerText)
+//     })
+// }
+
+// document.getElementById('ok').addEventListener('click', function(e) {
+//     document.getElementById('no').classList.add('toggleOn')
+// })
+
+document.getElementById('ok').addEventListener('click', function(e) {
+    let noElement = document.getElementById('no')
+    if (noElement.classList.contains('toggleOn')) {
+        noElement.classList.replace('toggleOn', 'toggleOff')
+    }
+    else if (noElement.classList.contains('toggleOff')) {
+        noElement.classList.replace('toggleOff', 'toggleOn')
+    }
+    else {
+        noElement.classList.add('toggleOn')
+    }
 })
-let eleS = document.getElementsByClassName('flex-item')
-for (let ele of eleS) {
-    ele.addEventListener('mousemove', function(e) {
-        console.log('x = ', e.screenX, " y = ",  e.screenY)
-    })
-    ele.addEventListener('click', function(e) {
-        alert(ele.innerText)
-    })
-}
+var borderTick = 33
+document.getElementById('cancel').style.border =`${borderTick}px solid white`
+let mar = 20
+mar = mar + 1
+document.getElementById('ok').addEventListener('click', function(e){
+    mar += 2
+    var marSize = `20px ${mar}px`
+    document.getElementById('cancel').style.margin = marSize
+    document.getElementById('no').style.margin = marSize
+})
