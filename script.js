b9204488e0d3c8e752010f87a2820f4d504faa4c
Chanakarn <chanakarn_h@cmu.ac.th>
2022-05-19 09:32:19 +0700

    name

---
+++ script.js
@@ -0,0 +1,5 @@
+var newText = 'new Text'
+document.getElementById('ok').innerHTML = newText
+
+var newName = 'Chanakarn'
+document.getElementById('no').innerHTML = '<bi><i>' + newName + '</i></bi>';
